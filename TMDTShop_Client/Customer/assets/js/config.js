31e3c65fd0acea0771fab5185543b33c33906c30
Lucas7Vincent <lbqm20032007@gmail.com>
2025-05-06 10:22:48 +0700

    update init branch minh

---
+++ TMDTShop_Client/Customer/assets/js/config.js
@@ -0,0 +1,12 @@
+// Firebase Configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDE2e4kVbx-FI1bZqNFPH7gPfyUHreYlgM",
+    authDomain: "shopx-a9954.firebaseapp.com",
+    projectId: "shopx-a9954",
+    storageBucket: "shopx-a9954.firebasestorage.app",
+    messagingSenderId: "318593412881",
+    appId: "1:318593412881:web:3f785d1d6ea2325bece6b0",
+    measurementId: "G-P5V81N7HY8"
+  };
+
+export { firebaseConfig }; 
